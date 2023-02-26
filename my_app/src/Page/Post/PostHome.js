632abe37e05bf9a8e328bac43583cd259c33725d
Kevin14Tsui <lungtsui15@gmial.com>
2023-02-26 17:23:04 +0800

    20CustomHook

--- my_app/src/Page/Post/PostHome.js
+++ my_app/src/Page/Post/PostHome.js
@@ -1,36 +1,14 @@
 import { useState, useEffect } from "react";
 import BlogList from "./PostList";
+import useFetch from "./useFetch";
 
 const PostHome = () => {
-  const [blogs, setBlogs] = useState(null);
-  const [isPending, setIsPending] = useState(true);
-  const [error, setError] = useState(null);
-
-  //   const handleDelete = (id) => {
-  //     const newBlogs = blogs.filter((blog) => blog.id !== id);
-  //     setBlogs(newBlogs);
-  //   };
-
-  useEffect(() => {
-    setTimeout(() => {
-      fetch("http://localhost:8000/blogs")
-        .then((res) => {
-          if (!res.ok) {
-            throw Error("could not fetch the data for the resource");
-          }
-          return res.json();
-        })
-        .then((data) => {
-          setBlogs(data);
-          setIsPending(false);
-          setError(null);
-        })
-        .catch((err) => {
-          setIsPending(false);
-          setError(err.message);
-        });
-    }, 1000);
-  }, []);
+  // call the hook
+  const {
+    data: blogs,
+    isPending,
+    error,
+  } = useFetch("http://localhost:8000/blogs");
 
   return (
     <div className="home">
