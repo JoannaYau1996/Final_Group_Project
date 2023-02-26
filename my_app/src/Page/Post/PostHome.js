79505d9933db6e2bc0d3cc8bb1eb28d042d4e7d3
Kevin14Tsui <lungtsui15@gmial.com>
2023-02-26 17:10:31 +0800

    19

--- my_app/src/Page/Post/PostHome.js
+++ my_app/src/Page/Post/PostHome.js
@@ -4,28 +4,37 @@ import BlogList from "./PostList";
 const PostHome = () => {
   const [blogs, setBlogs] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  //   const handleDelete = (id) => {
+  //     const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //     setBlogs(newBlogs);
+  //   };
 
   useEffect(() => {
     setTimeout(() => {
       fetch("http://localhost:8000/blogs")
         .then((res) => {
+          if (!res.ok) {
+            throw Error("could not fetch the data for the resource");
+          }
           return res.json();
         })
         .then((data) => {
-          console.log(data);
           setBlogs(data);
           setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setIsPending(false);
+          setError(err.message);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
