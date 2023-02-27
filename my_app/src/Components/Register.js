75874a312cb633e0878ff820ec6780f6d9d11453
Kevin14Tsui <lungtsui15@gmial.com>
2023-02-28 01:42:48 +0800

    KevinRegisterCss

--- my_app/src/Components/Register.js
+++ my_app/src/Components/Register.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
 function Register() {
   const navigate = useNavigate();
@@ -18,68 +20,91 @@ function Register() {
   };
 
   return (
-    <>
-      <h2>Create an account</h2>
+    <div className="container-fluid text-start green logincontainer w-50 py-5">
+      <h2 className="d-flex justify-content-center p-3 ">Create an account</h2>
 
       <form onSubmit={handleSubmitRegister}>
-        <div className="form-outline mb-4">
-          <input
-            name="name"
-            value={input.name}
-            onChange={(e) =>
-              setInput({ ...input, [e.target.name]: e.target.value })
-            }
-            type="text"
-            id="form3Example1cg"
-            className="form-control form-control-lg"
-          />
-          <label className="form-lable" htmlFor="form3Example1cg">
-            Your Name
-          </label>
-        </div>
+        <div className="">
+          <div className="form-outline mb-4">
+            <label
+              className="form-lable green fw-bold"
+              htmlFor="form3Example1cg"
+            >
+              Your Name
+            </label>
+            <input
+              required
+              minlength="3"
+              name="name"
+              value={input.name}
+              onChange={(e) =>
+                setInput({ ...input, [e.target.name]: e.target.value })
+              }
+              type="text"
+              id="form3Example1cg"
+              className="form-control form-control-lg "
+            />
+          </div>
+          <div className="form-outline mb-4">
+            <label
+              className="form-lable green fw-bold"
+              htmlFor="form3Example3cg"
+            >
+              Your Email
+            </label>
+            <input
+              required
+              name="email"
+              value={input.email}
+              onChange={(e) =>
+                setInput({ ...input, [e.target.name]: e.target.value })
+              }
+              type="email"
+              id="form3Example3cg"
+              className="form-control form-control-lg "
+            />
+          </div>
 
-        <div className="form-outline mb-4">
-          <input
-            name="email"
-            value={input.email}
-            onChange={(e) =>
-              setInput({ ...input, [e.target.name]: e.target.value })
-            }
-            type="email"
-            id="form3Example3cg"
-            className="form-control form-control-lg"
-          />
-          <label className="form-lable" htmlFor="form3Example3cg">
-            Your Email
-          </label>
-        </div>
+          <div className="form-outline mb-4">
+            <label
+              className="form-lable green fw-bold"
+              htmlFor="form3Example4cg"
+            >
+              Password
+            </label>
+            <input
+              required
+              minlength="4"
+              name="password"
+              value={input.password}
+              onChange={(e) =>
+                setInput({ ...input, [e.target.name]: e.target.value })
+              }
+              type="password"
+              id="form3Example4cg"
+              className="form-control form-control-lg"
+            />
+          </div>
 
-        <div className="form-outline mb-4">
-          <input
-            name="password"
-            value={input.password}
-            onChange={(e) =>
-              setInput({ ...input, [e.target.name]: e.target.value })
-            }
-            type="password"
-            id="form3Example4cg"
-            className="form-control form-control-lg"
-          />
-          <label className="form-lable" htmlFor="form3Example4cg">
-            Password
-          </label>
+          <Button
+            type="submit"
+            className="bg-dark green fw-bold p-1 border border-info rounded"
+          >
+            Register
+          </Button>
+          <hr />
+          <div className=" d-flex">
+            <p>Have already an account?</p>
+            <Link
+              to="/login"
+              className="d4d4 register_link ms-2 fw-bold text-decoration-none"
+            >
+              Login here
+            </Link>
+          </div>
         </div>
-
-        <div>
-          <button type="submit">Register</button>
-        </div>
-
-        <p>Have already an account?</p>
-        <Link to="/login">
-          <u>Login here</u>
-        </Link>
       </form>
-    </>
+    </div>
   );
 }
 
